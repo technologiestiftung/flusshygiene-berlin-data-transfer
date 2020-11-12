c5cb855323142b0e08600bb2b86f84bc6136f4c8
ff6347 <fabianmoronzirfas@protonmail.ch>
2020-11-12 11:50:47 +0100

    condtional change of process.env

--- __test__/jest-setup.js
+++ __test__/jest-setup.js
@@ -1,4 +1,13 @@
-process.env.S3_BUCKET = "";
-process.env.AWS_SECRET_ACCESS_KEY = "";
-process.env.AWS_ACCESS_KEY_ID = "";
+if (process.env.S3_BUCKET === undefined) {
+  process.env.S3_BUCKET = "";
+}
+if (process.env.AWS_SECRET_ACCESS_KEY === undefined) {
+  process.env.AWS_SECRET_ACCESS_KEY = "";
+}
+if (process.env.AWS_ACCESS_KEY_ID === undefined) {
+  process.env.AWS_ACCESS_KEY_ID = "";
+}
+if (process.env.TSB_SECRET === undefined) {
+  process.env.TSB_SECRET = "";
+}
 jest.useFakeTimers();
