f62ec310589f78cfc8142a6df8893807eceefd7f
ff6347 <fabianmoronzirfas@protonmail.ch>
2020-12-14 13:19:25 +0100

    add terraform for s3 bucket

---
+++ terraform/s3.tf
@@ -0,0 +1,59 @@
+resource "aws_s3_bucket" "uploads" {
+  bucket        = "${var.prefix}-${var.name}-${var.env}"
+  acl           = "public-read"
+  force_destroy = true
+  versioning {
+    enabled = false
+  }
+
+  policy = jsonencode({
+    "Version" = "2012-10-17"
+    "Id"      = "Policy-public-read-1"
+    "Statement" = [
+      {
+        "Sid"    = "AllowPublicRead"
+        "Effect" = "Allow"
+        "Principal" = {
+          "AWS" = "*"
+        }
+        "Action"   = "s3:GetObject"
+        "Resource" = "arn:aws:s3:::${var.prefix}-${var.name}-${var.env}/*"
+      }
+    ]
+  })
+
+  #  policy = <<POLICY
+  #  {
+  #    "Version": "2012-10-17",
+  #    "Id": "Policy-public-read-1",
+  #    "Statement": [
+  #        {
+  #            "Sid": "AllowPublicRead",
+  #            "Effect": "Allow",
+  #            "Principal": {
+  #                "AWS": "*"
+  #            },
+  #            "Action": "s3:GetObject",
+  #            "Resource": "arn:aws:s3:::${var.prefix}-${var.name}-${var.env}/*"
+  #        }
+  #    ]
+  #  }
+  #  POLICY
+  #  # Should be added for production
+  #cors_rule {
+  #  allowed_headers = ["*"]
+  #  allowed_methods = ["PUT", "POST", "GET"]
+  #  allowed_origins = "${var.allowed_origins}"
+
+  #  expose_headers  = ["ETag"]
+  #  max_age_seconds = 6000
+  #}
+
+  tags = {
+    name    = "terraform bucket for data from berliner wasser betriebe"
+    project = "flsshygn"
+    type    = "storage"
+  }
+
+}
+
