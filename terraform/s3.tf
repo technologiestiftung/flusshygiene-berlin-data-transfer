eecadd8f5c58dde14ae4d2a633c1dd9e108f9b98
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-05-18 15:06:19 +0200

    fix(CORS Headers): Allow CORS

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -40,14 +40,14 @@ resource "aws_s3_bucket" "uploads" {
   #  }
   #  POLICY
   #  # Should be added for production
-  #cors_rule {
-  #  allowed_headers = ["*"]
-  #  allowed_methods = ["PUT", "POST", "GET"]
-  #  allowed_origins = "${var.allowed_origins}"
+  cors_rule {
+   allowed_headers = ["*"]
+   allowed_methods = ["GET"]
+   allowed_origins = "${var.allowed_origins}"
 
-  #  expose_headers  = ["ETag"]
-  #  max_age_seconds = 6000
-  #}
+   expose_headers  = ["ETag"]
+   max_age_seconds = 6000
+  }
 
   tags = {
     name    = "terraform bucket for data from berliner wasser betriebe"
