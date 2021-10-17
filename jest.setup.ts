afd51f37bbafb6c85aca009d6cd87269b2529d4c
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-10-17 19:17:41 +0200

    fix all tests

---
+++ jest.setup.ts
@@ -0,0 +1,30 @@
+import { server } from "./__tests__/mocks/server";
+jest.useRealTimers();
+if (process.env.S3_BUCKET === undefined) {
+  process.env.S3_BUCKET = "";
+}
+if (process.env.AWS_SECRET_ACCESS_KEY === undefined) {
+  process.env.AWS_SECRET_ACCESS_KEY = "";
+}
+if (process.env.AWS_ACCESS_KEY_ID === undefined) {
+  process.env.AWS_ACCESS_KEY_ID = "";
+}
+if (process.env.TSB_SECRET === undefined) {
+  process.env.TSB_SECRET = "";
+}
+jest.useFakeTimers();
+
+beforeAll(() => {
+  // Enable the mocking in tests.
+  server.listen();
+});
+
+afterEach(() => {
+  // Reset any runtime handlers tests may use.
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  // Clean up once the tests are done.
+  server.close();
+});
