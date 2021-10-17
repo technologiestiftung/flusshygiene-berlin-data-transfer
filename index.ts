16af575de4936985ca80d7750240ce3e5b2bc514
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-10-17 17:58:09 +0200

    untangle promise chain

--- index.ts
+++ index.ts
@@ -1,6 +1,3 @@
-// @ts-check
-
-// import config from "./config.json";
 import { getLatestBWBFile } from "./lib/get-last-bwb-file";
 import moment from "moment";
 import { setupAWS, uploadAWS } from "./lib/aws";
@@ -10,6 +7,7 @@ import { csv } from "./lib/csv";
 import {
   csv2buffer,
   csv2json,
+  CSVRow,
   json2buffer,
   RawCSVRow,
   transform,
@@ -21,15 +19,15 @@ import { join } from "path";
 const config: { stations: string[] } = JSON.parse(
   readFileSync(join(__dirname, "./config.json"), "utf8")
 );
+const s3 = setupAWS();
+if (!("TSB_SECRET" in process.env)) {
+  throw Error("TSB_SECRET is required as environmental variable");
+}
 
 async function main() {
-  if (!("TSB_SECRET" in process.env)) {
-    throw Error("TSB_SECRET is required as environmental variable");
-  }
-  const s3 = setupAWS();
-
-  Promise.all(
-    config.stations.map(async (station) => {
+  // create filtered data tasks
+  const createFilteredDataForStationsTasks = config.stations.map(
+    async (station) => {
       const sreihe: "m" | "w" = "m";
       const smode = "c";
       const sdatum = moment().subtract(5, "day").format("DD.MM.YYYY");
@@ -54,72 +52,131 @@ async function main() {
       const transformedData = transform(cleanedData, sreihe);
       const date = moment().subtract(1, "day").format("YYYY-MM-DD");
       const filteredData = filterByDate(transformedData, date, "Datum");
-      const csvBuff = csv2buffer(filteredData);
-      await Promise.all([
-        uploadAWS(
-          s3,
-          csvBuff,
-          `stations/${station}/${moment().format("YYYY-MM-DD_hh-mm-ss")}.csv`
-        ),
-        uploadAWS(s3, csvBuff, `stations/${station}/latest.csv`),
-      ]);
-      const jsonBuff = json2buffer(csv2json(filteredData));
-      await Promise.all([
-        uploadAWS(
-          s3,
-          jsonBuff,
-          `stations/${station}/${moment().format("YYYY-MM-DD_hh-mm-ss")}.json`
-        ),
-        uploadAWS(s3, jsonBuff, `stations/${station}/latest.json`),
-      ]);
-      return Promise.resolve();
-    })
-  )
-    .then(async () => {
-      let data;
+      return { filteredData, station };
+    }
+  );
+  try {
+    const filteredDataSets = await Promise.all(
+      createFilteredDataForStationsTasks
+    );
 
-      try {
-        const url = await getLatestBWBFile();
-        data = await get(url);
-      } catch (err) {
-        console.error(err);
-        throw err;
+    const csvBuffers = filteredDataSets.map(
+      ({
+        filteredData,
+        station,
+      }: {
+        filteredData: CSVRow[];
+        station: string;
+      }) => {
+        const csvBuff = csv2buffer(filteredData);
+        return { buffer: csvBuff, station };
       }
+    );
+    const jsonBuffers = filteredDataSets.map(
+      ({
+        filteredData,
+        station,
+      }: {
+        filteredData: CSVRow[];
+        station: string;
+      }) => {
+        const jsonBuff = json2buffer(csv2json(filteredData));
+        return { buffer: jsonBuff, station };
+      }
+    );
 
-      const cleanedData = await csv<{ date: string; value: string }>(
-        extractAndCleanBwb(data).csvString,
-        "\t"
+    const csvUploadTasks = csvBuffers.map(({ buffer, station }) => {
+      const ulDated = uploadAWS(
+        s3,
+        buffer,
+        `stations/${station}/${moment().format("YYYY-MM-DD_hh-mm-ss")}.csv`
       );
-      const transformedData = transformBwb(cleanedData);
-      const date = moment().subtract(5, "day").format("YYYY-MM-DD");
-      const filteredData = filterByDate(transformedData, date, "date");
-      if (filteredData.length === 0) {
-        console.error("No bwb data found for the last 3 days");
-        return;
-      }
-      const csvBuff = csv2buffer(filteredData);
-      await Promise.all([
-        uploadAWS(
-          s3,
-          csvBuff,
-          `wastewater/${moment().format("YYYY-MM-DD_hh-mm-ss")}.csv`
-        ),
-        uploadAWS(s3, csvBuff, "wastewater/latest.csv"),
-      ]);
-      const jsonBuff = json2buffer(csv2json(filteredData));
-      await Promise.all([
-        uploadAWS(
-          s3,
-          jsonBuff,
-          `wastewater/${moment().format("YYYY-MM-DD_hh-mm-ss")}.json`
-        ),
-        uploadAWS(s3, jsonBuff, "wastewater/latest.json"),
-      ]);
-      return Promise.resolve();
-    })
-    .catch((err) => {
-      throw err;
+      const ulLatest = uploadAWS(s3, buffer, `stations/${station}/latest.csv`);
+      return [ulDated, ulLatest];
     });
+
+    const jsonUploadTasks = jsonBuffers.map(({ buffer, station }) => {
+      const ulDated = uploadAWS(
+        s3,
+        buffer,
+        `stations/${station}/${moment().format("YYYY-MM-DD_hh-mm-ss")}.json`
+      );
+      const ulLatest = uploadAWS(s3, buffer, `stations/${station}/latest.json`);
+      return [ulDated, ulLatest];
+    });
+
+    const csvFlat = csvUploadTasks.flat();
+    const jsonFlat = jsonUploadTasks.flat();
+    const csvResultWaPo = await Promise.allSettled(csvFlat);
+    const jsonResultWaPo = await Promise.allSettled(jsonFlat);
+    console.log("Wasserportal csv Upload Report");
+    console.log(csvResultWaPo);
+    console.log("Wasserportal json Upload Report");
+    console.log(jsonResultWaPo);
+  } catch (error) {
+    console.error(
+      error,
+      "Error getting data and uploading to s3 from wasserportal"
+    );
+  }
+
+  let data: string;
+
+  try {
+    const url = await getLatestBWBFile();
+    data = await get(url);
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+  const extractedAndCleandBWBData = extractAndCleanBwb(data);
+
+  const cleanedData = await csv<{ date: string; value: string }>(
+    extractedAndCleandBWBData.csvString,
+    "\t"
+  );
+  const transformedData = transformBwb(cleanedData);
+
+  const days = 1;
+  const date = moment().subtract(days, "day").format("YYYY-MM-DD");
+  const filteredData = filterByDate(transformedData, date, "date");
+
+  if (filteredData.length === 0) {
+    console.error(`No bwb data found for the last ${days} days`);
+    return;
+  }
+  const csvBuff = csv2buffer(filteredData);
+  const jsonBuff = json2buffer(csv2json(filteredData));
+
+  const csvBWBUploadTasks = () => {
+    const ulDated = uploadAWS(
+      s3,
+      csvBuff,
+      `wastewater/${moment().format("YYYY-MM-DD_hh-mm-ss")}.csv`
+    );
+    const ulLatest = uploadAWS(s3, csvBuff, `wastewater/latest.csv`);
+    return [ulDated, ulLatest];
+  };
+  const jsonBWBUploadTasks = () => {
+    const ulDated = uploadAWS(
+      s3,
+      jsonBuff,
+      `wastewater/${moment().format("YYYY-MM-DD_hh-mm-ss")}.json`
+    );
+    const ulLatest = uploadAWS(s3, jsonBuff, `wastewater/latest.json`);
+
+    return [ulDated, ulLatest];
+  };
+  try {
+    const jsonResultBwb = await Promise.allSettled(jsonBWBUploadTasks());
+    const csvResultBwb = await Promise.allSettled(csvBWBUploadTasks());
+    console.log("BWB csv Upload Report");
+    console.log(csvResultBwb);
+    console.log("BWB json Upload Report");
+    console.log(jsonResultBwb);
+  } catch (error) {
+    console.error(error, "Error getting data and uploading to s3 from bwb");
+  }
 }
 
 main().catch((err) => {
