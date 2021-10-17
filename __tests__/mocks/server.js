0ba5a0266a077e7e39f947fc204f01452bf7ba66
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-10-17 16:20:33 +0200

    move to ts

--- __tests__/mocks/server.js
+++
@@ -1,5 +0,0 @@
-// @ts-check
-const { setupServer } = require("msw/node");
-const handlers = require("./handlers");
-const server = setupServer(...handlers);
-module.exports = server;
--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  setupFilesAfterEnv: ["<rootDir>/__tests__/jest-setup.js"],
-  testPathIgnorePatterns: [
-    "<rootDir>/__tests__/mocks/",
-    "__tests__/jest-setup.js",
-  ],
-};
